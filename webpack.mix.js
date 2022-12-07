89ba9ee06c2e7ed3828cb0f14b9873e25193b0db
rocky kev <kev.rocky@gmail.com>
2022-12-06 21:48:44 -0800

    genearting sourcemaps

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,5 +3,15 @@ let mix = require('laravel-mix');
 mix
   // Scenario 1 - Combining Files
   // Scenario 2 - Minify and obfuscate [In production]
+  // Scenario 4 - Babel!
   .combine(['src/js/debug.js', 'src/js/script.js'], 'asset/js/index.js', true)
-  .setPublicPath('asset');
+
+  // Scenario 6 - Generating Sourcemaps
+  .sourceMaps(true, 'source-map')
+ 
+
+    // Scenario 3 - Vendor Prefixes
+    // Scenario 5 - Compiling SCSS
+  .sass('src/sass/style.scss', 'asset/css')
+   
+
