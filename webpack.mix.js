9831e64d2777cb4fafd98d5d69da4f4675d27b1a
rocky kev <kev.rocky@gmail.com>
2022-12-06 22:16:03 -0800

    scenario 8

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,28 @@
-let mix = require('laravel-mix');
+const path = require('path');
+const mix = require('laravel-mix');
+const LiveReloadPlugin = require('webpack-livereload-plugin');
+
+require('laravel-mix-eslint')
 
 mix
   // Scenario 1 - Combining Files
   // Scenario 2 - Minify and obfuscate [In production]
   // Scenario 4 - Babel!
   .combine(['src/js/debug.js', 'src/js/script.js'], 'asset/js/index.js', true)
+
+  // TODO: doesn't work yet
+  // Scenario 7 - Linting
+  .eslint({
+    fix: false,
+    context: './asset/js/index.js',
+    extensions: ['js', 'ts'],
+    emitWarning: true,
+    emitError: true,
+    emitWarning: true,
+    failOnWarning: false,
+    failOnError: false,
+    eslintPath: require.resolve('eslint')
+  })
 
   // Scenario 6 - Generating Sourcemaps
   .sourceMaps(true, 'source-map')
@@ -14,4 +32,11 @@ mix
     // Scenario 5 - Compiling SCSS
   .sass('src/sass/style.scss', 'asset/css')
    
+
+  // TODO: Test
+  // Scenario 8 - Hot-Reloading
+  .webpackConfig({
+    plugins: [new LiveReloadPlugin()]
+  });
+
 
